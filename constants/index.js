be34bfe75b5df0a7cfcb1265fdb88cb890fe8212
Talha Tufan <talha.tufan@hotmail.com>
2025-04-14 01:28:43 +0300

    Eklemeler yapıldı.

--- constants/index.js
+++ constants/index.js
@@ -26,3 +26,66 @@ export const categories = [
     },
   ];
   
+  export const featured = {
+    id: 1,
+    title: "Pizza",
+    description:"Birbirinden leziz pizzalar",
+    restaurants:[
+      {
+        id: 1,
+        name: 'PizzaBabası',
+        image: require('../assets/images/pizzaBabasi.png'),
+        description: 'Sıcak ve leziz pizzalar',
+        lng: 58.2145602,
+        lat: -25.5324269,
+        address: 'Baba Sokak , Ataşehir',
+        stars: 4.5,
+        reviews: '7.4k',
+        category: 'Fast Food',
+        dishes: [
+          {
+            id: 1,
+            name: 'pizza',
+            description: 'sucuk festivali',
+            price: 250,
+            image: require('../assets/images/pizzaDish.png'),
+          },
+          {
+            id: 2,
+            name: 'pizza',
+            description: 'karışık pizza',
+            price: 320,
+            image: require('../assets/images/pizzaDish.png'),
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: 'PizzaGuy',
+        image: require('../assets/images/pizzaGuy.png'),
+        description: 'Sıcak ve leziz pizzalar',
+        lng: 58.2145602,
+        lat: -25.5324269,
+        address: 'Genç Sokak , Kadıköy',
+        stars: 4.4,
+        reviews: '3.4k',
+        category: 'Fast Food',
+        dishes: [
+          {
+            id: 1,
+            name: 'pizza',
+            description: 'sucuk festivali',
+            price: 250,
+            image: require('../assets/images/pizzaDish.png'),
+          },
+          {
+            id: 2,
+            name: 'pizza',
+            description: 'karışık pizza',
+            price: 320,
+            image: require('../assets/images/pizzaDish.png'),
+          },
+        ],
+      }
+    ] 
+  }
