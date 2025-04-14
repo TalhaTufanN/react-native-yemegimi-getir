3cfb9d21fb43c6c8ad67a687692cba46c2fee1c1
Talha Tufan <talha.tufan@hotmail.com>
2025-04-14 19:04:21 +0300

    Restoran sayfası tasarlandı.

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,11 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Feather";
+import { themeColors } from "../theme/theme";
+import DishRow from "../components/dishRow";
+import InfoCart from "../components/infoCart";
 
 export default function RestaurantScreen() {
+  const { params } = useRoute();
+  const navigation = useNavigation();
+  let item = params;
+  // console.log('restaurant: ', item);
   return (
-    <SafeAreaView>
-      <Text>RestaurantScreen</Text>
-    </SafeAreaView>
-  )
+    <View className="flex-1">
+      <InfoCart/>
+      <ScrollView>
+        <View className="relative">
+          <Image className="w-full h-72" source={item.image} />
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            className="absolute top-10 left-4 bg-gray-50 p-2 rounded-full"
+          >
+            <Icon name="arrow-left" size={30} color={themeColors.bgColor(1)} />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{ borderTopLeftRadius: 30, borderTopRightRadius: 30 }}
+          className="bg-white -mt-12 pt-6"
+        >
+          <View className="px-5">
+            <Text className="text-3xl font-bold">{item.name}</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <Image
+                  className="h-4 w-4"
+                  source={require("../assets/images/star.png")}
+                />
+                <Text className="text-xs">
+                  <Text className="text-green-700">{item.stars}</Text>
+                  <Text className="text-gray-700">
+                    {} ({item.reviews}) -{" "}
+                    <Text className="font-semibold">{item.category}</Text>{" "}
+                  </Text>
+                </Text>
+              </View>
+              <View className="flex-row items-center space-x-1 px-3">
+                <Icon name="map-pin" size={15} />
+                <Text className="text-gray-700 text-xs">{item.address}</Text>
+              </View>
+            </View>
+            <Text className="text-gray-500 mt-2">{item.description}</Text>
+          </View>
+        </View>
+        <View className="pb-36 bg-white" >
+          <Text className="px-4 py-4 text-2xl font-bold">Menü</Text>
+          {/* Yemekler */}
+          {
+            item.dishes.map((dish,index)=> <DishRow item={{...dish}} key={index} />)
+          }
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
