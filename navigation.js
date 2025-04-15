3f5328e5b31aa257d26f3f310b3dc83ab9241ada
Talha Tufan <talha.tufan@hotmail.com>
2025-04-15 20:28:00 +0300

    CartScreen eklendi.

--- navigation.js
+++ navigation.js
@@ -1,17 +1,20 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import CartScreen from './screens/CartScreen';
+import { TransitionPresets } from '@react-navigation/stack';
+import OrderPreparingScreen from './screens/OrderPreparingScreen';
 const Stack = createNativeStackNavigator();
-
 export default function Navigation() {
     return (
         <NavigationContainer >
           <Stack.Navigator screenOptions = {{headerShown: false}}>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+            <Stack.Screen name="OrderPrepare" component={OrderPreparingScreen} />
+            <Stack.Screen name="Cart" component={CartScreen}/>
           </Stack.Navigator>
         </NavigationContainer>
       );
