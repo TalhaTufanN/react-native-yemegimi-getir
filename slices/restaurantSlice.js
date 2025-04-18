76ad571cf42f1600000f6e538054ef3048007324
Talha Tufan <talha.tufan@hotmail.com>
2025-04-18 23:01:18 +0300

    Bug Fix:Redux Toolkit ile ilgili hata düzeltildi.

--- slices/restaurantSlice.js
+++ slices/restaurantSlice.js
@@ -1,18 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const  initialState={
+  restaurant:null,
+}
 export const restaurantSlice = createSlice({
   name: "restaurant",
-  initialState: {
-    restaurant: null,
-  },
+  initialState,
   reducers: {
     setRestaurant: (state, action) => {
-      state.restaurant += action.payload;
+      state.restaurant = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
 export const { setRestaurant } = restaurantSlice.actions;
-export const selectRestaurant = (state) => state.restaurant.restaurant;
+export const selectRestaurant = state => state.restaurant.restaurant;
 export default restaurantSlice.reducer;
