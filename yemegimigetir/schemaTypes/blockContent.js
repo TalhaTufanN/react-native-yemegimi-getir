908d7f4ba4322ab72cdf3bacec25709abdbe4a8b
Talha Tufan <talha.tufan@hotmail.com>
2025-04-20 22:07:27 +0300

    Sanity eklendi.

---
+++ yemegimigetir/schemaTypes/blockContent.js
@@ -0,0 +1,33 @@
+export default {
+    title: 'Block Content',
+    name: 'blockContent',
+    type: 'array',
+    of: [
+      {
+        type: 'block',
+        styles: [
+          { title: 'Normal', value: 'normal' },
+          { title: 'H1', value: 'h1' },
+          { title: 'H2', value: 'h2' },
+          { title: 'H3', value: 'h3' },
+          { title: 'Quote', value: 'blockquote' },
+        ],
+        lists: [{ title: 'Bullet', value: 'bullet' }],
+        marks: {
+          decorators: [
+            { title: 'Strong', value: 'strong' },
+            { title: 'Emphasis', value: 'em' },
+          ],
+          annotations: [
+            {
+              name: 'link',
+              type: 'object',
+              title: 'Link',
+              fields: [{ name: 'href', type: 'url', title: 'URL' }],
+            },
+          ],
+        },
+      },
+    ],
+  }
+  
---
+++ yemegimigetir/schemaTypes/category.js
@@ -0,0 +1,10 @@
+export default {
+    name: 'category',
+    title: 'Category',
+    type: 'document',
+    fields: [
+      { name: 'title', title: 'Title', type: 'string' },
+      { name: 'description', title: 'Description', type: 'text' },
+    ],
+  }
+  
