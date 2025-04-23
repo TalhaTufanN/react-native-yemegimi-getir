69196bacfd847e2eeae0a19d3f72b65c2c8722cc
Talha Tufan <talha.tufan@hotmail.com>
2025-04-23 17:55:06 +0300

    Renk paletleri eklendi ve temanın değiştirilebilir olması sağlandı.

--- theme/theme.js
+++ theme/theme.js
@@ -1,4 +1,51 @@
-export const themeColors = {
-  text: "#FF6061",
-  bgColor: (opacity) => `rgba(205, 20, 10, ${opacity})`,
+const palette = [
+  {
+    // turkuaz
+    text: '#00CCBB',
+    bgColor: opacity => `rgba(0, 204, 187, ${opacity})`,
+  },
+  {
+    // gece mavisi
+    text: '#003366',
+    bgColor: opacity => `rgba(0, 51, 102, ${opacity})`,
+  },
+  {
+    // mercan
+    text: '#FF6F61',
+    bgColor: opacity => `rgba(255, 111, 97, ${opacity})`,
+  },
+  {
+    // lavanta
+    text: '#B57EDC',
+    bgColor: opacity => `rgba(181, 126, 220, ${opacity})`,
+  },
+  {
+    // zeytin yeşili
+    text: '#808000',
+    bgColor: opacity => `rgba(128, 128, 0, ${opacity})`,
+  },
+  {
+    // koyu gri
+    text: '#333333',
+    bgColor: opacity => `rgba(51, 51, 51, ${opacity})`,
+  }
+]
+
+// Başlangıçta tema indeksi
+let currentThemeIndex = 0;
+
+// Tema renklerini içeren nesne
+export let themeColors = {
+  ...palette[currentThemeIndex]
+};
+
+// Bir sonraki temaya geçiş yapan fonksiyon
+export const cycleTheme = () => {
+  // İndeksi bir artır, palette uzunluğuna ulaşınca sıfırla (döngüsel geçiş)
+  currentThemeIndex = (currentThemeIndex + 1) % palette.length;
+  // themeColors nesnesini güncelle
+  themeColors = {
+    ...palette[currentThemeIndex]
+  };
+  return themeColors;
 };
