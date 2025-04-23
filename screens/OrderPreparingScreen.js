772d20639cec823af30f610881a6f5a4834f62d9
Talha Tufan <talha.tufan@hotmail.com>
2025-04-23 15:51:08 +0300

    Gereksiz importlar silindi.

--- screens/OrderPreparingScreen.js
+++ screens/OrderPreparingScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View } from "react-native";
 import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import WebView from "react-native-webview";
