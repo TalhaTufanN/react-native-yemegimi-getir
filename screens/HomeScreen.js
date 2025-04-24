0cc45cd1ef72c72af7517375c8af5b48bc4ca633
Talha Tufan <talha.tufan@hotmail.com>
2025-04-24 03:00:19 +0300

    Ekranın en altına pay bırakıldı

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,10 @@
-import { View, Text, TextInput, ScrollView, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
@@ -8,23 +14,19 @@ import Categories from "../components/categories";
 import FeaturedRow from "../components/featuredRow";
 import { getFeaturedRestaurants } from "../api";
 
-
 export default function HomeScreen() {
-  
-  const [featuredRestaurants,setFeaturedRestaurants] = useState([]);
-  // Bu state, tema değişikliğini bileşene yansıtmak için
-  const [theme, setTheme] = useState({...themeColors});
-  
+  const [featuredRestaurants, setFeaturedRestaurants] = useState([]);
+  const [theme, setTheme] = useState({ ...themeColors });
+
   const handleThemeChange = () => {
     const newTheme = cycleTheme();
-    setTheme({...newTheme}); // Bileşeni yeniden render etmek için state'i güncelle
+    setTheme({ ...newTheme });
   };
-  useEffect(()=>{
-    getFeaturedRestaurants().then(data=>{
-      // console.log("API VERİSİ:  ", JSON.stringify(data, null, 2));
+  useEffect(() => {
+    getFeaturedRestaurants().then((data) => {
       setFeaturedRestaurants(data);
-    })
-  },[])
+    });
+  }, []);
   return (
     <SafeAreaView className="bg-slate-50">
       <StatusBar barStyle="dark-content" />
@@ -41,16 +43,17 @@ export default function HomeScreen() {
             <Text className="text-gray-500">İstanbul</Text>
           </View>
         </View>
-        <View
-          style={{ backgroundColor: themeColors.bgColor(1), borderRadius: 100 }}
-          className="p-3"
-        >
-          <TouchableOpacity
-            onPress={handleThemeChange}
-            >
+        <TouchableOpacity onPress={handleThemeChange}>
+          <View
+            style={{
+              backgroundColor: themeColors.bgColor(1),
+              borderRadius: 100,
+            }}
+            className="p-3"
+          >
             <Icon name="sliders" color="white" size={20} />
-          </TouchableOpacity>
-        </View>
+          </View>
+        </TouchableOpacity>
       </View>
       {/* Ana ekran içeriği */}
       <ScrollView
@@ -61,8 +64,7 @@ export default function HomeScreen() {
         <Categories />
         {/* Öne Çıkanlar*/}
         <View className="mt-5">
-          {
-          featuredRestaurants.map((item, index) => {
+          {featuredRestaurants.map((item, index) => {
             return (
               <FeaturedRow
                 key={index}
@@ -70,10 +72,11 @@ export default function HomeScreen() {
                 description={item.description}
                 restaurants={item.restaurants}
               />
-            )
-          })
-          }
+            );
+          })}
         </View>
+        {/* En alt */}
+        <View className="flex-row items-center space-x-2 mt-1 px-3 mr-14 p-6 bg-slate-50" />
       </ScrollView>
     </SafeAreaView>
   );
